7ce43be10f147ff68f13955c9279cff406a63f91
aliz <aliz>
2021-06-27 07:46:51 +0800

    create .prj for each test

--- cpld/generateprj.py
+++ cpld/generateprj.py
@@ -1,8 +1,9 @@
 import xml.etree.ElementTree as ET
+
+
+# Load the project file, and make a list of implementation and simulation files.
 tree = ET.parse('beebkb.xise')
 root = tree.getroot()
-
-#print (root[0])
 
 implFiles = []
 testFiles = []
@@ -16,12 +17,21 @@ for filetag in root.findall('{http://www.xilinx.com/XMLSchema}files/{http://www.
 		assoc = assoctag.attrib['{http://www.xilinx.com/XMLSchema}name']
 		
 		if assoc == 'Implementation':
-			implFiles.append("vhdl work \"" + filename + "\"")
+			implFiles.append(filename)
 		elif assoc == 'BehavioralSimulation':
 			testFiles.append(filename)
+		elif assoc == 'PostRouteSimulation':
+			pass
+		else:
+			raise Exception("Unrecognised file association '" + assoc + "'")
 
-with open('all.prj', 'w') as f:
-	f.writelines("\n".join(implFiles))
+for testfile in testFiles:
+	# Create a .prj file, containing all the implementation files plus this simulation file.
+	with open(testfile + '.prj', 'w') as f:
+		for implFile in implFiles:
+			f.write("vhdl work \"" + implFile + "\"\n")
+		f.write("vhdl work \"" + testfile + "\"\n")
+	
+	print(testfile)
 
-for x in testFiles:
-	print(x)
+
