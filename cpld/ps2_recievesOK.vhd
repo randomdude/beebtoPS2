ec7364826bc9285477f123d3542317ccc13402f5
aliz <aliz>
2021-09-05 12:25:53 +0800

    reformat spaces to tabs

--- cpld/ps2_recievesOK.vhd
+++ cpld/ps2_recievesOK.vhd
@@ -3,12 +3,12 @@ USE ieee.std_logic_1164.ALL;
 
 library work;
 use work.testVectors.all;
- 
+
 ENTITY ps2_recievesOK IS
 END ps2_recievesOK;
  
 ARCHITECTURE behavior OF ps2_recievesOK IS 
- 
+
 	COMPONENT ps2ToBeeb
 	PORT(
 		fast_clk : IN std_logic;
@@ -24,28 +24,24 @@ ARCHITECTURE behavior OF ps2_recievesOK IS
 		);
 	END COMPONENT;
 
-   --Inputs
-   signal fast_clk : std_logic := '0';
-   signal ps2_clk : std_logic := '0';
-   signal ps2_data : std_logic := '0';
-   signal beeb_clk : std_logic := '0';
+	signal fast_clk : std_logic := '0';
+	signal ps2_clk : std_logic := '0';
+	signal ps2_data : std_logic := '0';
+	signal beeb_clk : std_logic := '0';
+	signal beeb_row : std_logic_vector(2 downto 0);
+	signal beeb_col : std_logic_vector(3 downto 0);
+	signal beeb_keydown : std_logic;
 
- 	--Outputs
-   signal beeb_row : std_logic_vector(2 downto 0);
-   signal beeb_col : std_logic_vector(3 downto 0);
-   signal beeb_keydown : std_logic;
-
-   constant fast_clk_period : time := 20 ns;
-   constant beeb_clk_period : time := 1000 ns;
+	constant fast_clk_period : time := 20 ns;
+	constant beeb_clk_period : time := 1000 ns;
 BEGIN
  
-	-- Instantiate the Unit Under Test (UUT)
-   uut: ps2ToBeeb PORT MAP (
-          ps2_clk => ps2_clk, ps2_data => ps2_data, 
-          fast_clk => fast_clk, beeb_clk => beeb_clk,
-          beeb_row => beeb_row, beeb_col => beeb_col,
-          beeb_keydown => beeb_keydown
-        );
+	uut: ps2ToBeeb PORT MAP (
+		ps2_clk => ps2_clk, ps2_data => ps2_data, 
+		fast_clk => fast_clk, beeb_clk => beeb_clk,
+		beeb_row => beeb_row, beeb_col => beeb_col,
+		beeb_keydown => beeb_keydown
+	);
 
 	-- The 1MHz clock the beeb provides
 	beeb_clk_process :process
@@ -61,9 +57,8 @@ BEGIN
 		fast_clk <= '1'; wait for fast_clk_period/2;
 	end process;
 
-   -- Stimulus process
-   stim_proc: process
-   begin		
+	stim_proc: process
+	begin
 		-- idle the line
 		ps2_clk <= '1'; ps2_data <= '1';
 		
@@ -71,11 +66,11 @@ BEGIN
 		work.testVectors.ps2_keydown_spacebar(ps2_clk, ps2_data);
 
 		-- We should see this key pressed.
-		assert beeb_keydown = '1' severity FAILURE;		
-		assert beeb_row = "110" severity FAILURE;		
-		assert beeb_col = "0010" severity FAILURE;		
+		assert beeb_keydown = '1' severity FAILURE;
+		assert beeb_row = "110" severity FAILURE;
+		assert beeb_col = "0010" severity FAILURE;
 
 		report "all OK" severity FAILURE;
-   end process;
+	end process;
 
 END;
--- cpld/signalsInterrupt.vhd
+++ cpld/signalsInterrupt.vhd
@@ -9,72 +9,62 @@ END signalsInterrupt;
  
 ARCHITECTURE behavior OF signalsInterrupt IS 
  
-    COMPONENT kb
-    PORT(
-         ROW : IN  std_logic_vector(2 downto 0);
-         COL : IN  std_logic_vector(3 downto 0);
-         CA2 : OUT  std_logic;
-         W : OUT  std_logic;
-         CB : IN  std_logic;
---         RST : OUT  std_logic;
-			
+	COMPONENT kb
+		PORT(
+			ROW : IN  std_logic_vector(2 downto 0);
+			COL : IN  std_logic_vector(3 downto 0);
+			CA2 : OUT  std_logic;
+			W : OUT  std_logic;
+			CB : IN  std_logic;
 			startupOptions: in STD_LOGIC_VECTOR(7 downto 0);
 
-			
-         beeb_clk : IN  std_logic;
-         ps2_clk  : IN  std_logic;
-         ps2_data : IN  std_logic
-        );
-    END COMPONENT;
-    
+			beeb_clk : IN  std_logic;
+			ps2_clk  : IN  std_logic;
+			ps2_data : IN  std_logic
+			);
+	END COMPONENT;
 
-   --Inputs
-   signal ROW : std_logic_vector(2 downto 0) := (others => '0');
-   signal COL : std_logic_vector(3 downto 0) := (others => '0');
-   signal CB : std_logic := '0';
+	signal ROW : std_logic_vector(2 downto 0) := (others => '0');
+	signal COL : std_logic_vector(3 downto 0) := (others => '0');
+	signal CB : std_logic := '0';
 	signal startupOptions: STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
 
-   signal beeb_clk  : std_logic := '0';
-   signal ps2_clk : std_logic := '0';
-   signal ps2_data: std_logic := '0';
+	signal beeb_clk  : std_logic := '0';
+	signal ps2_clk : std_logic := '0';
+	signal ps2_data: std_logic := '0';
+	signal CA2 : std_logic;
+	signal W : std_logic;
 
- 	--Outputs
-   signal CA2 : std_logic;
-   signal W : std_logic;
---   signal RST : std_logic;
-
-   constant beeb_clk_period : time := 1000 ns;
-   constant fast_clk_period : time := 100 ns;
+	constant beeb_clk_period : time := 1000 ns;
+	constant fast_clk_period : time := 100 ns;
  
 	signal sawInterrupt: integer;
 BEGIN
  
-   uut: kb PORT MAP (
-          ROW => ROW, COL => COL,
-          CA2 => CA2, W => W,
-          CB => CB,
---			 RST => RST,
-			 startupOptions => startupOptions,
-          beeb_clk => beeb_clk,
-			 ps2_clk  => ps2_clk, ps2_data => ps2_data
-        );
-		  
+	uut: kb PORT MAP (
+		ROW => ROW, COL => COL,
+		CA2 => CA2, W => W,
+		CB => CB,
+		startupOptions => startupOptions,
+		beeb_clk => beeb_clk,
+		ps2_clk  => ps2_clk, ps2_data => ps2_data
+	);
+
 	-- the beeb clock runs at 1mhz constantly
-   clk_process :process
-   begin
+	clk_process :process
+	begin
 		beeb_clk <= '0'; wait for beeb_clk_period/2;
 		beeb_clk <= '1'; wait for beeb_clk_period/2;
-   end process;
+	end process;
 
-   fastclk_process :process
-   begin
+	fastclk_process :process
+	begin
 		startupOptions(6) <= '0'; wait for fast_clk_period/2;
 		startupOptions(6) <= '1'; wait for fast_clk_period/2;
-   end process;
-		  
+	end process;
 
-   stim_proc: process
-   begin
+	stim_proc: process
+	begin
 		ps2_clk <= '1';
 		
 		-- When CB is high, we're in free-scanning mode. Row and column should be ignored.
@@ -104,6 +94,6 @@ BEGIN
 		assert sawInterrupt = 1 severity FAILURE;
 		
 		report "all OK" severity FAILURE;
-   end process;
+	end process;
 
 END;
